5a0be9b865a5ff8bffb6ab0db857d2f03cee90a6
vignesh <vignesh.narayanan@mediaplex.co.in>
2018-09-08 12:17:35 +0530

    1 build Delete

--- routes/register.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var register = require('../v1/controllers/register.controller');
-
-
-router.post('/addregister',  register.addregister);
-router.get('/listregister', register.listregister);
-
-module.exports = router;
